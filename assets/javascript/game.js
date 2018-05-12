06860144ca43c2a277bb35f3dd94a7336a3ad92d
Mick Burgess <mburgess@reliaslearning.com>
2018-05-12 02:49:43 -0400

    added more logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,38 @@
-$(document).ready(function () {
+
 // VARIABLES
-
-// random number to be used for each game
-var randnumber = Math.floor(Math.random() * 102 + 19);
-
-// place randnumber into the random-number div
-$("#random-number").text(randnumber);
 
 // wins
 var wins = 0;
+$("#wins").text("Wins: " + wins);
 
-// loses
-var loses = 0;
-
+// losses
+var losses = 0;
+$("#losses").text("Losses: " + losses);
 // your score
 var score = 0;
 
-$("#current-score").text(score);
-var isValueSet = false;
 
-// place randnumber into the random-number div
-$("#random-number").text(randnumber);
+
+// Used for intial score at the start of games
+function initialScore () {
+  score = 0;
+  $("#current-score").text(score);
+}
+initialScore();
+
+// Used for setting the number to aim for at the start of games
+function randomnumber () {
+  // random number to be used for each game
+  var randnumber = Math.floor(Math.random() * 102 + 19);
+  // place randnumber into the random-number div
+  $("#random-number").text(randnumber);
+}
+randomnumber();
 
 function setGemValue () {
   var gemValue = [];
   for (i = 0; i < 4; i++) {
   var randGemValue = Math.floor(Math.random() * 12 + 1);  
-  console.log("rand", randGemValue);
   gemValue.push(randGemValue);
   }
   
@@ -50,4 +56,31 @@ function setGemValue () {
 }
 setGemValue();
 
+$("input").click(function() {
+  var gemScore = ($(this).val());
+  gemScore = parseInt(gemScore);
+
+  score += gemScore;
+  $("#current-score").text(score);
+
+  if (score === randnumber) {
+    wins++;
+    $("#wins").text("Wins: " + wins);
+    setGemValue();
+    randomnumber();
+    initialScore();
+  }
+  else if (score < randnumber) {
+    return false;
+  }
+  else { 
+    losses++;
+    $("#losses").text("Losses: " + losses);
+    setGemValue();
+    randomnumber();
+    initialScore();
+  }
+  
 });
+
+  
